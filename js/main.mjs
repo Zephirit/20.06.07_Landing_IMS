b74dcbd559739725e48d812a563aa6ebe875e2f7
Katser <gray84@tut.by>
2020-06-08 14:13:10 +0300

    Сделал прокрутку страница, делается секция сравнения

--- js/main.mjs
+++ js/main.mjs
@@ -1,30 +1,70 @@
 window.onload = () => {
 	console.log("Страница загружена")
-	function _sendEmail(){
-		const forms = document.querySelectorAll('.form')
-		forms.forEach(form => {
-			form.onsubmit()
+
+	function _sendEmail() {
+		const forms = document.querySelectorAll('.form__input')
+		forms.forEach(frm => {
+			frm.onsubmit = (e) => {
+				e.preventDefault()
+				fetch('../send.php', {
+					method: 'POST',
+					body: new FormData(frm)
+				})
+			}
 		});
 	}
-	// $("#callBackContent").submit(function (e) {
-	// 	//устанавливаем событие отправки для формы
-	// 	e.preventDefault();
-	// 	var form_data = $(this).serialize(); //собераем все данные из формы
-	// 	$.ajax({
-	// 		type: "POST", //Метод отправки
-	// 		url: "/send.php", //путь до php фаила отправителя
-	// 		data: form_data,
-	// 		success: function success() {
-	// 			//код в этом блоке выполняется при успешной отправке сообщения
-	// 			alert("Ваше сообщение отправлено!");
-	// 			$(".callBackContentInput").val("");
-	// 		}
-	// 	});
-	// });
-	// $(".catalogLink").click(function () {
-	// 	$("#popupCatalog").fadeIn();
-	// });
-	// $(".popupCatalog__exit").click(function () {
-	// 	$("#popupCatalog").fadeOut();
-	// });
+
+	function _sectionsPosition() {
+		let sections = {}
+		document.querySelectorAll('.section').forEach(section => {
+			let topPos = section.getBoundingClientRect().top + pageYOffset
+			sections[section.id] = {}
+			sections[section.id].top = topPos
+		});
+		console.log(sections)
+
+		let _getTop = (id) => {
+			console.log(id)
+			return sections[id].top
+		}
+		return {
+			getTop: _getTop
+		}
+	}
+
+	function _scrollSection(target) {
+		const element = document.querySelector('#' + target)
+		const targetPos = _sectionsPosition().getTop(target)
+		console.log(element, targetPos)
+		if (target === 'description') {
+			const menu = document.querySelector('.descriptionMenu')
+			const menuHeight = menu.clientHeight
+			window.scroll({
+				top: targetPos - menuHeight,
+				behavior: 'smooth'
+			})
+		} else {
+			window.scroll({
+				top: targetPos,
+				behavior: 'smooth'
+			})
+		}
+
+	}
+
+	function _getLinks() {
+		document.querySelectorAll('.link').forEach(link => {
+			link.addEventListener('click', (e) => {
+				e.preventDefault()
+				const target = link.getAttribute('data-target')
+				_scrollSection(target)
+			})
+		})
+	}
+
+	function init() {
+		_sendEmail()
+		_getLinks()
+	}
+	init()
 }
